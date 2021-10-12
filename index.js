7269bfa3caa80c5d837c87cc2e0a20e63294b5eb
LancelotLewis <lancelot_lewis@163.com>
2021-10-12 17:08:13 +0800

    fix: action

--- index.js
+++ index.js
@@ -9,47 +9,49 @@ async function sendSlackMessage(webhook, msg) {
   }
 }
 
-try {
-  await exec.exec('pwd');
-  await exec.exec('ls');
-  console.log('Deploy to abtnode using github action');
-  const endpoint = core.getInput('endpoint');
-  const accessKey = core.getInput('access-key');
-  const accessSecret = core.getInput('access-secret');
-  const slackWebhook = core.getInput('slack-webhook');
+(async () => {
+  try {
+    await exec.exec('pwd');
+    await exec.exec('ls');
+    console.log('Deploy to abtnode using github action');
+    const endpoint = core.getInput('endpoint');
+    const accessKey = core.getInput('access-key');
+    const accessSecret = core.getInput('access-secret');
+    const slackWebhook = core.getInput('slack-webhook');
 
-  const file = path.join(process.cwd(), '.blocklet/release/blocklet.json');
-  if (!fs.existsSync(file)) {
-    throw new Error('Missing file at .blocklet/release/blocklet.json');
-  }
+    const file = path.join(process.cwd(), '.blocklet/release/blocklet.json');
+    if (!fs.existsSync(file)) {
+      throw new Error('Missing file at .blocklet/release/blocklet.json');
+    }
 
-  const version = require(file).version;
-  const name = require(file).name;
+    const version = require(file).version;
+    const name = require(file).name;
 
-  await exec.exec(
-    `blocklet deploy .blocklet/bundle --endpoint ${endpoint} --access-key ${accessKey} --access-secret ${accessSecret} --skip-hooks`,
-    {
-      listeners: {
-        stderr(err) {
-          sendSlackMessage(
-            slackWebhook,
-            JSON.stringify({
-              text: `:x: Faild to deploy ${name} v${version} to ${endpoint}`,
-            }),
-          );
-          throw new Error(err.toString());
+    await exec.exec(
+      `blocklet deploy .blocklet/bundle --endpoint ${endpoint} --access-key ${accessKey} --access-secret ${accessSecret} --skip-hooks`,
+      {
+        listeners: {
+          stderr(err) {
+            sendSlackMessage(
+              slackWebhook,
+              JSON.stringify({
+                text: `:x: Faild to deploy ${name} v${version} to ${endpoint}`,
+              }),
+            );
+            throw new Error(err.toString());
+          },
         },
       },
-    },
-  );
-  sendSlackMessage(
-    slackWebhook,
-    JSON.stringify({
-      text: `${name} v${version} was successfully deployed to ${endpoint}`,
-    }),
-  );
+    );
+    sendSlackMessage(
+      slackWebhook,
+      JSON.stringify({
+        text: `${name} v${version} was successfully deployed to ${endpoint}`,
+      }),
+    );
 
-  console.log(`Deploy blocklet ${name} to ${endpoint} success!`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    console.log(`Deploy blocklet ${name} to ${endpoint} success!`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
